adea72e554b7857c747bddd651901b3c82933d42
Victória Rose <70824102+EternalQuasar0206@users.noreply.github.com>
2022-04-14 10:36:14 -0300

    chore: remove console output due to compatibility problems

--- MelonJs.JavaScript/Bindings/console.js
+++ MelonJs.JavaScript/Bindings/console.js
@@ -1,19 +1,14 @@
 const console = {
-    output: [],
     log: (object) => {
-        console.output.push(object);
         melon_internal_console_log(object, 15);
     },
     error: (object) => {
-        console.output.push(object);
         melon_internal_console_log('[X] ' + object, 12);
     },
     warn: (object) => {
-        console.output.push(object);
         melon_internal_console_log('[!] ' + object, 14);
     },
-    clear: (clearOutput = false) => {
-        clearOutput ? output = [] : {}
+    clear: () => {
         melon_internal_console_clear();
     }
 };
