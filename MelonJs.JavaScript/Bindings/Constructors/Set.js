3b58424dc88f26037f20542176a3c3e757594bd1
Victória Rose <70824102+EternalQuasar0206@users.noreply.github.com>
2022-04-15 14:33:16 -0300

    chore: Finished Set constructor default methods

--- MelonJs.JavaScript/Bindings/Constructors/Set.js
+++ MelonJs.JavaScript/Bindings/Constructors/Set.js
@@ -11,4 +11,32 @@
             });
         }
     }
+
+    entries() {
+        return this._entries.map(x => [x, x]);
+    }
+
+    has(value) {
+        return this._entries.indexOf(value) != -1;
+    }
+
+    add(value) {
+        if (!this.has(value)) {
+            this.size++;
+            this._entries.push(value);
+        }
+    }
+
+    delete(value) {
+        if (this.has(value)) this.size--;
+        this.set(value, null);
+    }
+
+    clear() {
+        this._entries = [];
+    }
+
+    forEach(callback) {
+        this._entries.forEach(callback);
+    }
 }
