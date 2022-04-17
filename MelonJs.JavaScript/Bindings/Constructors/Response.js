febc9db479cc45aafc549e7a0d02319189fb48b8
Victória Rose <70824102+EternalQuasar0206@users.noreply.github.com>
2022-04-17 17:57:05 -0300

    chore: updates in http and changed from HttpResponse to Response

---
+++ MelonJs.JavaScript/Bindings/Constructors/Response.js
@@ -0,0 +1,17 @@
+﻿class Response {
+    constructor(body, headers, latency, statusCode, ok) {
+        this.body = body;
+        this.headers = headers;
+        this.latency = latency;
+        this.statusCode = statusCode;
+        this.ok = ok;
+    }
+
+    json() {
+        return JSON.parse(this.body);
+    }
+
+    text() {
+        return this.body;
+    }
+}
