8a70df64842bf9b1fed13bd2b32f91be73884fe5
Victória Rose <70824102+EternalQuasar0206@users.noreply.github.com>
2022-05-10 18:02:10 -0300

    chore: load function wip

--- MelonJs.JavaScript/Bindings/Tools/load.js
+++ MelonJs.JavaScript/Bindings/Tools/load.js
@@ -1,3 +1,15 @@
 ﻿function load(path) {
+    if (!Array.isArray(path)) {
+        const content = fs.read(path);
+        return eval(content);
+    }
 
+    const results = [];
+
+    path.forEach(x => {
+        const content = fs.read(path);
+        results.push(eval(content));
+    });
+
+    return results;
 }
