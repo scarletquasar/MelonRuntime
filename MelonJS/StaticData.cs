8a8dd56cc1c8fab7f9388afbffb23a2bb5ce1bee
Victória Rose <70824102+EternalQuasar0206@users.noreply.github.com>
2022-05-13 16:26:37 -0300

    Update StaticData.cs

--- MelonJS/StaticData.cs
+++ MelonJS/StaticData.cs
@@ -4,6 +4,6 @@ namespace MelonJS
 {
     public static class StaticData
     {
-        public static string ApplicationData() => $"MelonJS v{Assembly.GetExecutingAssembly().GetName().Version} (Testing Alpha)";
+        public static string ApplicationData() => $"MelonJS v{Assembly.GetExecutingAssembly().GetName().Version}";
     }
 }
