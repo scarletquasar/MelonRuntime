a4af69bc2712d456a3b556bc8b0d48e56b748bf1
Victória Rose <70824102+EternalQuasar0206@users.noreply.github.com>
2022-05-17 20:05:46 -0300

    fix hello world

--- MelonJs.Experiments/HelloWorld/index.js
+++ MelonJs.Experiments/HelloWorld/index.js
@@ -1,9 +1,3 @@
-const queueItems = [() => true, () => true, () => true];
-const queue = new Queue(queueItems);
-
-console.log(queue.run(true));
-
-
 environment.setVariable("hw","Hello World!")
 console.log(environment.getVariable("hw"))
 
