e1345bef7d99d7e7e1eb803a71ee036054399193
Victória Rose <70824102+EternalQuasar0206@users.noreply.github.com>
2022-05-17 10:02:52 -0300

    chore: File constructor wip

--- MelonJs.JavaScript/Bindings/Constructors/FileSystem/File.js
+++ MelonJs.JavaScript/Bindings/Constructors/FileSystem/File.js
@@ -1,9 +1,15 @@
 ﻿class File {
+    //Path
     path = null;
+
+    //File Information
     fileName = null;
     fileSize = null;
     lastWriteTime = null;
     creationTime = null;
+
+    //Object Limits
+    fileSizeLimit = 1000;
 
     bytes = new ArrayBuffer();
 }
