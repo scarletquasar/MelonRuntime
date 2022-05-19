2081f159af6c1b2bb547ec439909b63a58728f1a
Victória Rose <70824102+EternalQuasar0206@users.noreply.github.com>
2022-05-18 21:57:57 -0300

    chore: File c# object wip

--- MelonJs.Models/FileSystem/File.cs
+++ MelonJs.Models/FileSystem/File.cs
@@ -4,5 +4,13 @@
     {
         public string? Name { get; set; }
         public string? Content { get; set; }
+        public byte[]? Bytes { get; set; }
+        public string? Encoding { get; set; }
+        public DateTime? CreationTime { get; set; }
+        public DateTime? LastWriteTime { get; set; }
+        public long FileSizeLimit { get; set; }
+        public string[]? NotAllowedFileNameExpressions { get; set; }
+        public string[]? NotAllowedFileExtensions { get; set; }
+        public string? Path { get; set; }
     }
 }
