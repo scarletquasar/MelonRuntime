a4fe79fb15d5a572e52c19e52a6467b33a68d7a6
Victória Rose <70824102+EternalQuasar0206@users.noreply.github.com>
2022-05-19 13:57:34 -0300

    chore: Folder constructor updates

--- MelonJs.JavaScript/Bindings/Constructors/FileSystem/Folder.js
+++ MelonJs.JavaScript/Bindings/Constructors/FileSystem/Folder.js
@@ -1,11 +1,30 @@
 ﻿class Folder {
-    constructor(folderInfo = { name: "", content: [] /* File() array */ }) {
+    constructor(folderInfo = { name: "", content: [] }) {
+        content.forEach(file => {
+            if (!(file instanceof File)) {
+                this._errValidFiles();
+            }
+        });
+
         this.folderName = folderInfo.name;
         this.content = folderInfo.content;
+    }
+
+    add(file) {
+        if (file instanceof File) {
+            this.content.push(file);
+            return;
+        }
+
+        this._errValidFiles();
     }
 
     save(path) {
         melon_internal_create_folder(path);
         content.forEach(file => file.save(path + file.fileName));
+    }
+
+    _errValidFiles() {
+        throw new Error("Only valid files can be added to a Folder object.");
     }
 }
