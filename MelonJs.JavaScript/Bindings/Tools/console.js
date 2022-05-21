59b65c27c82be8c7e4794c648fa48220d2c7bee9
Victória Rose <70824102+EternalQuasar0206@users.noreply.github.com>
2022-05-20 23:16:38 -0300

    feat: console.table

--- MelonJs.JavaScript/Bindings/Tools/console.js
+++ MelonJs.JavaScript/Bindings/Tools/console.js
@@ -17,5 +17,29 @@ const console = {
 
     read: () => {
         return melon_internal_conrole_readLine();
+    },
+
+    table: (object) => {
+        let res = [];
+
+        if (Array.isArray(object)) {
+            object.forEach(item => {
+                res.push("|" + item + "|");
+            })
+        }
+
+        switch (typeof object) {
+            case "object":
+                Object.entries(object).forEach(entry => {
+                    res.push("|" + entry[0] + "|" + entry[1] + "|");
+                });
+                break;
+
+            default:
+                result.push("|" + object + "|");
+                break;
+        }
+
+        res.forEach(console.log);
     }
 };
