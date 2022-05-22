60cb3574fe53e94484457bc17bb18bf8d4413dc7
Victória Rose <70824102+EternalQuasar0206@users.noreply.github.com>
2022-05-22 16:32:24 -0300

    feat(wip): MelonFileManager
    - Model to preserve DRY-based arch and englobe file management methods with detailed instructions

---
+++ MelonJs.Static/Tools/FileManagement/MelonFileManager.cs
@@ -0,0 +1,10 @@
+﻿namespace MelonJs.Static.Tools.FileManagement
+{
+    public static class MelonFileManager
+    {
+        public static void Copy(string from, string to)
+        {
+
+        }
+    }
+}
