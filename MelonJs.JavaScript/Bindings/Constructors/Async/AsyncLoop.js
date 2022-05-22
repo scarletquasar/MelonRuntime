bbcbf781e7c63b30a1f0ef0d81aba89f651902ad
Victória Rose <70824102+EternalQuasar0206@users.noreply.github.com>
2022-05-22 14:04:12 -0300

    feat(wip): AsyncLoop / chore: load() declared hotfix

---
+++ MelonJs.JavaScript/Bindings/Constructors/Async/AsyncLoop.js
@@ -0,0 +1,28 @@
+﻿class AsyncLoop {
+    constructor(action, times, delay, errorAction = console.error) {
+        this._stopped = false;
+
+        this.stop = () => this._stopped = true;
+
+        this.times = times;
+
+        this._promise = new Promise(
+            resolve => {
+                if (delay > 0)
+                    setTimeout(() => resolve(action()), delay)
+
+                resolve(action());
+            }, errorAction)
+
+        this.execute = () => {
+            Promise.resolve(this._promise)
+                .then(() => {
+                    if ((this.times > 0 || this.times < 0) && !this._stopped) {
+                        this.execute();
+                        this.times--;
+                    }
+                })
+                .catch(errorAction)
+        }
+    }
+}
