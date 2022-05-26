3c6b06ca1dc08cca4a4d14ec8f3b548deeeb326f
Victória Rose <70824102+EternalQuasar0206@users.noreply.github.com>
2022-05-25 21:18:01 -0300

    feat(wip): Enumerable constructor

---
+++ MelonJs.JavaScript/Bindings/Constructors/Enumerable.js
@@ -0,0 +1,17 @@
+﻿function Enumerable(base) {
+    this._elements = Array.isArray(base) ? base : []
+    this.all = () => this.elements
+
+    this.where = (filter) => new Enumerable(this._elements).filter(filter)
+    this.top = (quantity) => new Enumerable(this._elements.slice(0, quantity))
+    this.bottom = (quantity) => new Enumerable(this._elements.reverse()).top(quantity)
+    this.first = () => this._elements[0]
+    this.last = () => new Enumerable(_elements.reverse()).first()
+    this.average = () => this._elements.reduce((partialSum, a) => partialSum + a) / this._elements.length
+    this.all = (condition) => {
+        const boolArray = this._elements.map(x => condition(x))
+        return boolArray.every(true)
+    }
+
+    return this;
+}
