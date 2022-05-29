8b1f780817dfb8da653ea0ddd603f8b83899fc2e
Victória Rose <70824102+EternalQuasar0206@users.noreply.github.com>
2022-05-29 16:21:34 -0300

    fix: import module to execute

--- projects/melon-runtime/commands/melon.js
+++ projects/melon-runtime/commands/melon.js
@@ -1,4 +1,4 @@
 #! /usr/bin/env node
-import { execSync } from 'child_process'
+const execSync = require('child_process').execSync
 
 code = execSync('dotnet run --configuration Release --project ../../../projects/melon-runtime/MelonJS/MelonJS.csproj')
