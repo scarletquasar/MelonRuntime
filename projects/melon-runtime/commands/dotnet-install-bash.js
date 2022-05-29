dee9ae1681362fd05da15c0aca355acccefc0d63
Victória Rose <70824102+EternalQuasar0206@users.noreply.github.com>
2022-05-29 03:23:09 -0300

    removed dotnet-install scripts

--- projects/melon-runtime/commands/dotnet-install-bash.js
+++
@@ -1,3 +0,0 @@
-import { execSync } from 'child_process'
-
-execSync('../../../scripts/linux/dotnet-install.sh')
--- projects/melon-runtime/commands/dotnet-install-win.js
+++
@@ -1,3 +0,0 @@
-import { execSync } from 'child_process'
-
-execSync('../../../scripts/windows/dotnet-install.ps1', {'shell':'powershell.exe'})
