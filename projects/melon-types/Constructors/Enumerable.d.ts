553f785444ff86af7caa2c55fc2ee1433b1748de
Victória Rose <70824102+EternalQuasar0206@users.noreply.github.com>
2022-06-05 20:21:42 -0300

    chore: Enumerable constructor types

--- projects/melon-types/Constructors/Enumerable.d.ts
+++ projects/melon-types/Constructors/Enumerable.d.ts
@@ -1,4 +1,17 @@
-declare type Enumerable = {
-    elements: any[],
-    where: (filter: (this: any) => boolean) => Enumerable,
+declare type Enumerable<T> = {
+    elements: () => T[],
+    where: (filter: (this: T) => boolean) => Enumerable<T> ,
+    top: (quantity: number) => Enumerable<T> ,
+    bottom: (quantity: number) => Enumerable<T>,
+    first: () => T,
+    last: () => T,
+    average: () => T,
+    any: () => boolean,
+    cast: (constructor: any) => Enumerable<any>,
+    all: (condition: (this: T) => boolean) => boolean,
+    add: (item: T) => void,
+    addRange: (items: T[] | Enumerable<T>) => void,
+    lookFor: (element: T) => any,
+    compare: (element: T, compFn: (this: T) => boolean) => boolean,
+    equals: (target: any) => boolean
 }
