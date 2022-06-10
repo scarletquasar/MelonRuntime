19b45dafe1ef9abba6ea5cb278151f1d4115f733
Victória Rose <70824102+EternalQuasar0206@users.noreply.github.com>
2022-06-10 11:46:55 -0300

    chore: fixed require function and removed unused null in JintStatic

--- projects/melon-runtime/MelonJs.JavaScript/Bindings/Tools/require.js
+++ projects/melon-runtime/MelonJs.JavaScript/Bindings/Tools/require.js
@@ -1,13 +1,11 @@
 function require(module) {
-    console.log(module)
-
     const parts = module.split(":")
 
     if(!(parts[0].endsWith(".js"))) {
         parts[0] = parts[0] += ".js"
     }
 
-    const loaded = load(__basedir__ + path)
+    const loaded = load(__basedir__ + parts[0])
 
     if(parts[1]) {
         return loaded[parts[1]]
