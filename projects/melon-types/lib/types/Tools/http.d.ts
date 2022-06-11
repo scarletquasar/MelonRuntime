97e05e41df8f8fbbc45c5eb952feca8cd5fb0580
Victória Rose <70824102+EternalQuasar0206@users.noreply.github.com>
2022-06-11 16:26:15 -0300

    fixed melon-types typings + minor version

--- projects/melon-types/lib/types/Tools/http.d.ts
+++ projects/melon-types/lib/types/Tools/http.d.ts
@@ -5,7 +5,7 @@ type Http = {
     request: (target: string, method: string, body: string, headers: string) => MResponse,
     ping: (target: string, times: number) => PingResponse,
     app: (host?: string, port?: string, enableHttps?: boolean) => HttpApplication,
-    result: (statusCode: number, response?: Record<any, any>) => {
+    result: (statusCode: number, response?: any) => {
         statusCode: number,
         response: string
     }
