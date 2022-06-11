934322aa7e44dc84da0804a46fc3f24bb6a91e87
Victória Rose <70824102+EternalQuasar0206@users.noreply.github.com>
2022-06-11 16:37:11 -0300

    resources: Fixed project generator readme resources

--- projects/melon-runtime/MelonJS/Properties/Resources.Designer.cs
+++ projects/melon-runtime/MelonJS/Properties/Resources.Designer.cs
@@ -121,8 +121,8 @@ namespace MelonJS.Properties {
         ///
         ///| Command | Description |
         ///| ------- | ----------- |
-        ///| `nm i melon-runtime -g` | Installs MelonJS (requires .NET 6 SDK and Runtime) |
-        ///| `npm install` | (Only TypeScript) - Installs the required MelonJS types |
+        ///| `npm i melon-runtime -g` | Installs MelonJS (requires .NET 6 SDK and Runtime) |
+        ///| `npm install` | Installs the required MelonJS types |
         ///| `npm run go` | Run the project |.
         /// </summary>
         internal static string NewProjectReadme {
