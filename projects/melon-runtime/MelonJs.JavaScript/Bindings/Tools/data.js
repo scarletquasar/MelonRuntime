519cf17c00fd7f560a59331fd9df57fc2d4aad45
Victória Rose <70824102+EternalQuasar0206@users.noreply.github.com>
2022-06-13 00:50:55 -0300

    refactor(wip): [data] life quality changes

--- projects/melon-runtime/MelonJs.JavaScript/Bindings/Tools/data.js
+++ projects/melon-runtime/MelonJs.JavaScript/Bindings/Tools/data.js
@@ -1,36 +1,28 @@
-﻿const data = {}
-
-data["clone"] = (target) => {
-    if (debug.enableDetailedInformation) {
-        //Reference case for https://stackoverflow.com/questions/122102/what-is-the-most-efficient-way-to-deep-clone-an-object-in-javascript
-        if (typeof object === 'function') {
-            debug.log(`
-                [MelonJS Debugger]: deep_clone may be slow and cause data loss due to the usage of
-                JSON.parse() and JSON.stringify()
-
-                Example:
-
-                const a = {
-                  string: 'string',
-                  number: 123,
-                  bool: false,
-                  nul: null,
-                  date: new Date(),  // stringified
-                  undef: undefined,  // lost
-                  inf: Infinity,  // forced to 'null'
-                  re: /.*/,  // lost
-                }
-
-                console.log(a);
-                console.log(typeof a.date);  // Date object
-                const clone = JSON.parse(JSON.stringify(a));
-                console.log(clone);
-                console.log(typeof clone.date);  // result of .toISOString()
-            `)
+﻿const data = {
+    /*
+     * data.clone(object)
+     * Performs non-structural cloning of an object (currently using the eval-stringify-parse technique 
+     * not indicated, in the future it will be replaced by a structural data cloning)
+     * */
+    clone: function (target) {
+        if (debug.enableDetailedInformation) {
+            //Reference case for https://stackoverflow.com/questions/122102/what-is-the-most-efficient-way-to-deep-clone-an-object-in-javascript
+            debug.log
+                ("[Debug]: deep_clone may be slow and cause data loss due to the usage of " +
+                 "JSON.parse() and JSON.stringify()")
         }
+
+        if (typeof object === 'function') {
+            debug.log
+                ("[Debug]: Cloning functions is actually a workaround and may not work as intended");
+
+            return function () { target(...arguments) }
+        }
+
+        return JSON.parse(JSON.stringify(eval(target)));
     }
-    return JSON.parse(JSON.stringify(eval(target)));
 }
+
 data["compare"] = (value1, value2, cmpFn) => {
     if (cmpFn === void 0) { cmpFn = function (a, b) { return a === b; }; }
     var firstConstructor = value1.constructor.name;
