e430b4d2103603c29e99aa2a88e561af90bc2e51
Victória Rose <70824102+EternalQuasar0206@users.noreply.github.com>
2022-06-18 18:49:28 -0300

    chore: data types to implement MySQL and SQL Server providers

--- projects/melon-types/lib/types/Tools/data.d.ts
+++ projects/melon-types/lib/types/Tools/data.d.ts
@@ -10,6 +10,14 @@ type Data = {
     PgClient: (host: string, port: number, database: string, username: string, password: string) => {
         executeNonQuery: (sql: string) => number,
         executeQuery: (sql: string) => any
+    },
+    MySQLClient: (host: string, port: number, database: string, username: string, password: string) => {
+        executeNonQuery: (sql: string) => number,
+        executeQuery: (sql: string) => any
+    },
+    SqlServerClient: (host: string, port: number, database: string, username: string, password: string) => {
+        executeNonQuery: (sql: string) => number,
+        executeQuery: (sql: string) => any
     }
 }
 
