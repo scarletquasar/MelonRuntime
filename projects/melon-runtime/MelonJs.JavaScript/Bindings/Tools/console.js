72f0542d81c611772f500f8d6615a1339f141704
Victória Rose <70824102+EternalQuasar0206@users.noreply.github.com>
2022-06-19 13:14:32 -0300

    chore: console.table re-implemenation

--- projects/melon-runtime/MelonJs.JavaScript/Bindings/Tools/console.js
+++ projects/melon-runtime/MelonJs.JavaScript/Bindings/Tools/console.js
@@ -56,28 +56,94 @@ const console = {
      * console.table()
      * Displays tabular data as a table
      * */
-    table: function (object) {
-        let res = []
-
-        if (Array.isArray(object)) {
-            object.forEach(item => {
-                res.push(`|${item}|`)
-            })
-            return
-        }
-        else {
-            switch (typeof object) {
-                case "object":
-                    Object.entries(object).forEach(entry => {
-                        res.push(`|${entry[0]}|${entry[1]}|`)
-                    })
-                    break
-
-                default:
-                    res.push(`|${object}|`)
-                    break
+    table: function (data, columns) {
+        if (data.constructor === Object) {
+            if (!columns) {
+                var columns = [];
+                for (var prop in data[0]) {
+                    if (columns.indexOf(prop) === -1) {
+                        columns.push(prop);
+                    }
                 }
+
+                var header = '(index)';
+                for (var p in columns) {
+                    header += ' | ';
+                    header += columns[p];
+                }
+                console.log(header);
+
+            } else if (typeof columns !== 'object') {
+                var columns = [];
+                for (var index in data) {
+                    for (var prop in data[index]) {
+                        if (columns.indexOf(prop) === -1) {
+                            columns.push(prop);
+                        }
+                    }
+                }
+            } else {
+                var header = '(index)';
+                for (var p in columns) {
+                    header += ' | ';
+                    header += columns[p];
+                }
+                console.log(header);
+            }
+
+            for (var obj in data) {
+                var entry = data[obj];
+                var entryStr = obj + '';
+                for (var j = 0; j < columns.length; j++) {
+                    entryStr += ' | ';
+                    entryStr += entry[columns[j]];
+                }
+                console.log(entryStr);
+            }
+
+        } else if (data.constructor === Array) {
+            if (!columns) {
+                var columns = [];
+                for (var prop in data[0]) {
+                    if (columns.indexOf(prop) === -1) {
+                        columns.push(prop);
+                    }
+                }
+
+                var header = '(index)';
+                for (var p in columns) {
+                    header += ' | ';
+                    header += columns[p];
+                }
+                console.log(header);
+
+            } else if (typeof columns !== 'object') {
+                var columns = [];
+                for (var index in data) {
+                    for (var prop in data[index]) {
+                        if (columns.indexOf(prop) === -1) {
+                            columns.push(prop);
+                        }
+                    }
+                }
+            } else {
+                var header = '(index)';
+                for (var p in columns) {
+                    header += ' | ';
+                    header += columns[p];
+                }
+                console.log(header);
+            }
+
+            for (var i = 0; i < data.length; i++) {
+                var entry = data[i];
+                var entryStr = i + '';
+                for (var j = 0; j < columns.length; j++) {
+                    entryStr += ' | ';
+                    entryStr += entry[columns[j]];
+                }
+                console.log(entryStr);
+            }
         }
-        res.forEach(console.log)
     }
 }
