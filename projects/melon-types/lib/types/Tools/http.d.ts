2a6db7e9a08578ef9e4c4f55e898036d41413f43
vilanz <>
2022-06-29 02:11:12 -0300

    feat: more image and audio extensions

--- projects/melon-types/lib/types/Tools/http.d.ts
+++ projects/melon-types/lib/types/Tools/http.d.ts
@@ -2,8 +2,8 @@
 /// <reference path="../Constructors/MResponse.d.ts" />
 /// <reference path="../Constructors/HttpApplication.d.ts" />
 
-type ImageExtension = "png" | "gif" | "jpeg" | "jpg" | "png" | "svg+xml" | "webp"
-type AudioExtension = "wave" | "wav" | "x-wav" | "x-pn-wav" | "webm" | "ogg"
+type ImageExtension = "png" | "gif" | "jpeg" | "jpg" | "png" | "svg+xml" | "webp" | "bmp" | "avif" | "tiff"
+type AudioExtension = "wave" | "wav" | "x-wav" | "x-pn-wav" | "webm" | "ogg" | "opus" | "midi" | "aac"
 type VideoExtension = "x-flv" | "mp4" | "x-msvideo" | "mpeg" | "ogg" | "webm" | "mp2t" | "3gpp" | "3ggp2"
 
 type MSimpleResponse<R, MT extends string> = {
