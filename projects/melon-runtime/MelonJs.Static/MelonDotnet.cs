f775cd5213489af1c64e42b4ab10c033a5cd19a4
Victória Rose <70824102+EternalQuasar0206@users.noreply.github.com>
2022-07-02 13:05:54 -0300

    chore: implemented caching in MelonDotnet
    Warning: experimental - currently using [lock] operator to prevent unexcepted operation cache changes in multi threaded environments
    
    The changes does not affect the current MelonDotnet workflow and usage

--- projects/melon-runtime/MelonJs.Static/MelonDotnet.cs
+++ projects/melon-runtime/MelonJs.Static/MelonDotnet.cs
@@ -1,62 +1,110 @@
 ﻿using System.Reflection;
-
 namespace MelonJs.Static
 {
     public static class MelonDotnet
     {
+        private static Tuple<string, List<Type>> _cachedMethodSearchTypes = new("", new());
+        private static Tuple<string, List<Type>> _cachedFieldSearchTypes = new("", new());
+        private static Tuple<string, List<Type>> _cachedInstanceCreationTypes = new("", new());
         public static dynamic? CallMethod(string nSpace, string search, string methodName, object[] parameters)
         {
             Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
 
-            var type =
-                assemblies
-                .Select(assembly => assembly.GetTypes())
-                .SelectMany(x => x)
-                .Where(x => x.Namespace == nSpace)
-                .Where(x => x.FullName.Contains(search))
-                .First();
+            IEnumerable<Type> types;
 
-            var method = type
+            if(_cachedMethodSearchTypes.Item1 != search)
+            {
+                types =
+                    assemblies
+                    .Select(assembly => assembly.GetTypes())
+                    .SelectMany(x => x)
+                    .Where(x => x.Namespace == nSpace)
+                    .Where(x => x.FullName != null && x.FullName.Contains(search));
+
+                lock(_cachedMethodSearchTypes!)
+                {
+                    _cachedMethodSearchTypes = new(search, types.ToList());
+                }
+            }
+            else
+            {
+                types = _cachedMethodSearchTypes.Item2;
+            }
+
+            IQueryable<MethodInfo> method =
+                types
+                .First()
                 .GetMethods()
                 .Where(x => x.Name == methodName)
-                .FirstOrDefault();
+                .AsQueryable();
 
-            return method?.Invoke(parameters[0], parameters.Skip(1).ToArray());
+            var result = method.FirstOrDefault();
+
+            return result?.Invoke(parameters[0], parameters.Skip(1).ToArray());
         }
 
         public static dynamic? GetField(string nSpace, string search, string fieldName)
         {
             Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
 
-            var type =
-                assemblies
-                .Select(assembly => assembly.GetTypes())
-                .SelectMany(x => x)
-                .Where(x => x.Namespace == nSpace)
-                .Where(x => x.FullName!.Contains(search))
-                .First();
+            IEnumerable<Type> types;
 
-            var field = type
+            if (_cachedFieldSearchTypes.Item1 != search)
+            {
+                types =
+                    assemblies
+                    .Select(assembly => assembly.GetTypes())
+                    .SelectMany(x => x)
+                    .Where(x => x.Namespace == nSpace)
+                    .Where(x => x.FullName != null && x.FullName.Contains(search));
+
+                lock (_cachedFieldSearchTypes!)
+                {
+                    _cachedFieldSearchTypes = new(search, types.ToList());
+                }
+            }
+            else
+            {
+                types = _cachedFieldSearchTypes.Item2;
+            }
+
+            IQueryable<FieldInfo> field = types
+                .First()
                 .GetFields()
                 .Where(x => x.Name == fieldName)
-                .FirstOrDefault();
+                .AsQueryable();
 
-            return field;
+            var result = field.FirstOrDefault();
+
+            return result;
         }
 
         public static dynamic CreateInstanceOfType(string nSpace, string search, object[] parameters)
         {
             Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
 
-            var type =
+            IEnumerable<Type> types;
+
+            if (_cachedInstanceCreationTypes.Item1 != search)
+            {
+                types =
                 assemblies
                 .Select(assembly => assembly.GetTypes())
                 .SelectMany(x => x)
                 .Where(x => x.Namespace == nSpace)
-                .Where(x => x.FullName!.Contains(search))
-                .First();
+                .Where(x => x.FullName != null && x.FullName.Contains(search));
 
-            return Activator.CreateInstance(type!, parameters, null)!;
+                lock (_cachedInstanceCreationTypes!)
+                {
+                    _cachedInstanceCreationTypes = new(search, types.ToList());
+                }
+            }
+            else
+            {
+                types = _cachedInstanceCreationTypes.Item2;
+            }
+
+            return Activator.CreateInstance(types!.First(), parameters, null)!;
         }
     }
 }
