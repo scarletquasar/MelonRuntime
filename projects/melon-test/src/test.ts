036d2a0871eb822d794af85711019b06f660bcee
Victória Rose <70824102+EternalQuasar0206@users.noreply.github.com>
2022-07-07 16:04:06 -0300

    chore: implemented test result to be a promise

--- projects/melon-test/src/test.ts
+++ projects/melon-test/src/test.ts
@@ -2,14 +2,14 @@ class Test {
     description: string;
     private _assertions: boolean[];
     add: (value: boolean) => void;
-    result: (log?: boolean) => boolean;
+    result: (log?: boolean) => Promise<boolean>;
 
     constructor(description: string) {
         this._assertions = [];
         this.description = description;
         this.add = (value: boolean) => void this._assertions.push(value);
-        this.result = (log = false) => {
-            const passed = this._assertions.every((assertion) => assertion === true);
+        this.result = async (log = false) => {
+            const passed = Promise.resolve(this._assertions.every((assertion) => assertion === true));
            
             if(log) {
                 console.log(`${passed ? "[Ok]" : "[Failed]"} ${this.description}`);
