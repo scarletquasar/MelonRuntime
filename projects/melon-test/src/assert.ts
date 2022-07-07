cee3d6726d1c171a5701ac30c805b0096ff05cb7
Victória Rose <70824102+EternalQuasar0206@users.noreply.github.com>
2022-07-07 02:37:58 -0300

    chore: test api updates

--- projects/melon-test/src/assert.ts
+++ projects/melon-test/src/assert.ts
@@ -5,6 +5,10 @@ const assert = {
     falsy: (target: any) => !Boolean(target),
     null: (target: any) => target === null,
     notNull: (target: any) => target !== null,
-    isEmpty: (target: any) => target.length === 0,
-    isNotEmpty: (target: any[]) => target.length > 0
+    arrayIsEmpty: (target: any) => target.length === 0,
+    arrayIsNotEmpty: (target: any[]) => target.length > 0,
+    objectIsEmpty: (target: Record<any, any>) => Object.keys(target).length === 0,
+    objectIsNotEmpty: (target: Record<any, any>) => Object.keys(target).length > 0,
+    equals: (target1: any, target2: any) => data.compare(target1, target2, (x: any, y: any) => x === y),
+    notEquals: (target1: any, target2: any) => data.compare(target1, target2, (x: any, y: any) => x !== y),
 }
