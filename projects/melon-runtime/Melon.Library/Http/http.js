1e2ad43a855b17e64a53209ef51239c143e4cd8b
Victória Rose <70824102+EternalQuasar0206@users.noreply.github.com>
2022-07-11 23:22:03 -0300

    chore: implemented http.request migration

--- projects/melon-runtime/Melon.Library/Http/http.js
+++ projects/melon-runtime/Melon.Library/Http/http.js
@@ -42,6 +42,23 @@
             })
         }
     },
+    Response: class {
+        constructor(body, headers, latency, statusCode, ok) {
+            this.body = body;
+            this.headers = headers;
+            this.latency = latency;
+            this.statusCode = statusCode;
+            this.ok = ok;
+        }
+
+        json() {
+            return JSON.parse(this.body);
+        }
+
+        text() {
+            return this.body;
+        }
+    },
     app: (options = { host: "localhost", port: 80, enableHttps: false }) => {
         const { HttpApplication } = http;
         const host = options.host;
@@ -51,4 +68,42 @@
         http._apps.push(new HttpApplication(host, port, enableHttps));
         return http._apps[http._apps.length - 1];
     },
+    request: function (target, method = "GET", body = "{}", headers = "{}") {
+        return new AsyncTask((target, method, body, headers) => {
+            typeof headers === "object" ? headers = JSON.stringify(headers) : {};
+            typeof body === "object" ? body = JSON.stringify(body) : {};
+
+            const rawResult = _$internalBinding["FetchRequest"](target, method, body, headers);
+
+            return new http.Response(
+                rawResult.Body ?? "",
+                rawResult.Headers ?? {},
+                rawResult.Latency ?? 0,
+                rawResult.StatusCode ?? 599,
+                rawResult.Ok ?? false
+            )
+        }, [target, method, body, headers], 0).execute();
+    },
+    result: (statusCode, response = {}) => {
+        return {
+            type: "application/json",
+            status: statusCode,
+            response: JSON.stringify(response)
+        }
+    },
+    static: (response, type) => {
+        return {
+            type,
+            status: 200,
+            response
+        }
+    },
+    image: (response, extension) => {
+        extension = extension === "jpg" ? "jpeg" : extension;
+        return http.static(response, `image/${extension}`);
+    },
+    audio: (response, extension) => http.static(response, `audio/${extension}`),
+    video: (response, extension) => http.static(response, `video/${extension}`),
+    pdf: (response) => http.static(response, 'application/pdf'),
+    html: (response) => http.static(response, 'text/html')
 }
---
+++ projects/melon-runtime/Melon.Library/Models/PingReply.cs
@@ -0,0 +1,29 @@
+﻿namespace Melon.Library.Models
+{
+    public class PingReply
+    {
+        public PingReply(List<float> results)
+        {
+            if (results != null)
+            {
+                MaxLatency = results.Max();
+                MinLatency = results.Min();
+                AverageLatency = results.Average();
+
+                Results = results;
+                return;
+            }
+
+            MaxLatency = 0;
+            MinLatency = 0;
+            AverageLatency = 0;
+
+            Results = new();
+        }
+
+        public List<float> Results { get; set; }
+        public float AverageLatency { get; set; }
+        public float MaxLatency { get; set; }
+        public float MinLatency { get; set; }
+    }
+}
