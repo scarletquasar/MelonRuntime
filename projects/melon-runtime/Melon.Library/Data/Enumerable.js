898f7e79367a5f2145958e267fd5b98f7f221060
Victória Rose <70824102+victoriaquasar@users.noreply.github.com>
2022-07-14 21:16:43 -0300

    feat(tests): Enumerable constructor tests implementation + Enumerable fixes
    - Implemented Enumerable constructor tests using xunit
    - Several Enumerable constructor workflow fixes

--- projects/melon-runtime/Melon.Library/Data/Enumerable.js
+++ projects/melon-runtime/Melon.Library/Data/Enumerable.js
@@ -7,14 +7,14 @@
             throw new Error(this._errorCapacity);
         }
 
-        if (!this[index] && typeof this[index] !== "boolean") {
+        if (this[index] === undefined) {
             this[index] = value;
             this.count++;
             return;
         }
 
         index++;
-        this.add(value, index)
+        this.add(value, index);
     }
 
     /* Setup array-based structure */
@@ -35,7 +35,7 @@
         const result = [];
 
         while (index < this.count) {
-            if (this[index] || typeof this[index] === "boolean") {
+            if (this[index] != undefined) {
                 result.push(this[index]);
             }
 
@@ -47,24 +47,32 @@
 
     /* Setup methods */
     this.take = (quantity) => new Enumerable(this.toArray().slice(0, quantity));
-    this.skip = (quantity) => new Enumerable(this.toArray().slice(quantity, 0));
-    this.where = (filter) => new Enumerable(this.toArray()).filter(filter);
-    this.firstOrDefault = () => this[0] ?? null;
-    this.lastOrDefault = () => this[this.count - 1] ?? null;
+    this.skip = (quantity) => new Enumerable(this.toArray().slice(quantity));
+    this.where = (filter) => new Enumerable(this.toArray().filter(filter));
+    this.firstOrDefault = () => this.toArray()[0] ?? null;
+    this.lastOrDefault = () => this.toArray()[this.count - 1] ?? null;
     this.first = () => this[0];
     this.last = () => this[this.count - 1];
     this.any = () => this.toArray().length > 0;
-    this.all = (condition = x => x === x) => this.toArray().map(x => condition(x)).every(true);
-    this.average = () => this.toArray().reduce((partialSum, a) => partialSum + a) / this._elements.length;
+    this.all = (condition = x => x === x) => this.toArray().every(x => condition(x));
+
+    this.average = () => {
+        const array = this.toArray();
+        const sum = array.reduce((a, b) => a + b, 0);
+
+        return (sum / array.length) || 0;
+    }
     this.cast = (constructor) => new Enumerable(this.toArray().map(x => new constructor(x)));
     this.equals = (element) => data.compare(this.toArray(), element);
     this.addRange = (elements) => elements.forEach(this.add);
+
     this.clear = () => {
         let index = this.count;
         while(index > 0) {
             this[index - 1] = undefined;
             index--;
         }
+        this.count = 0;
     }
 
     /* Return instance */
