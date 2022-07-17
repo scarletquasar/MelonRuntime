656a001ef1011339759742b65df027514b182109
Victória Rose <70824102+victoriaquasar@users.noreply.github.com>
2022-07-16 22:02:58 -0300

    chore: Std library tests

--- projects/melon-runtime/Melon.Tests/ScriptLibrary/Standards/StdLibraryTests.cs
+++ projects/melon-runtime/Melon.Tests/ScriptLibrary/Standards/StdLibraryTests.cs
@@ -58,7 +58,7 @@ namespace Melon.Tests.ScriptLibrary.Standards
         }
 
         [Fact(DisplayName = "'std' system.environment.currentDirectory() method return should be valid")]
-        public void StdSystemEnvironmentCurrentDirectoryShouldBeValid()
+        public void StdEnvironmentCurrentDirectoryShouldBeValid()
         {
             var script = @"std.environment.currentDirectory()";
             var result = _engine.Evaluate(script).AsString();
@@ -67,12 +67,71 @@ namespace Melon.Tests.ScriptLibrary.Standards
         }
 
         [Fact(DisplayName = "'std' system.environment.baseDirectory() method return should be valid")]
-        public void StdSystemEnvironmentBaseDirectoryShouldBeValid()
+        public void StdEnvironmentBaseDirectoryShouldBeValid()
         {
             var script = @"std.environment.baseDirectory()";
             var result = _engine.Evaluate(script).AsString();
 
             Assert.NotEmpty(result);
+        }
+
+        [Fact(DisplayName = "'std' system.environment.getEnvironmentVariables() method return should be valid")]
+        public void StdEnvironmentGetEnvironmentVariablesShouldBeValid()
+        {
+            var script = @"
+                std.environment.setEnvironmentVariable('test', 123);
+                Object.keys(std.environment.getEnvironmentVariables()).length;
+            ";
+            var result = _engine.Evaluate(script).AsNumber();
+
+            Assert.NotEqual((uint)0, (uint)result);
+        }
+
+        [Fact(DisplayName = "'std' system.environment.setEnvironmentVariable() method return should be valid")]
+        public void StdEnvironmentSetEnvironmentVariableShouldBeValid()
+        {
+            var script = @"
+                std.environment.setEnvironmentVariable('test', 123);
+                std.environment.getEnvironmentVariables()['test'];
+            ";
+            var result = _engine.Evaluate(script).AsNumber();
+
+            Assert.Equal(123, result);
+        }
+
+        [Fact(DisplayName = "'std' system.environment.clearLocalEnvironmentVariables() method return should be valid")]
+        public void StdEnvironmentClearLocalEnvironmentVariablesShouldBeValid()
+        {
+            var script = @"
+                std.environment.setEnvironmentVariable('test', 123);
+                std.environment.clearLocalEnvironmentVariables();
+                Object.keys(std.environment.getEnvironmentVariables()).length;
+            ";
+            var result = _engine.Evaluate(script).AsNumber();
+
+            Assert.Equal((uint)0, (uint)result);
+        }
+
+        [Fact(DisplayName = "'std' system.process.argv should be a valid array")]
+        public void StdProcessArgvShouldveAValidArray()
+        {
+            var script = @"
+                Array.isArray(std.process.argv);
+            ";
+            var result = _engine.Evaluate(script).AsBoolean();
+
+            Assert.True(result);
+        }
+
+        [Fact(DisplayName = "'std' system.process.env should be a valid object")]
+        public void StdProcessEnvShouldveAValidObject()
+        {
+            var script = @"
+                std.process.env instanceof Object;
+            ";
+            var result = _engine.Evaluate(script).AsBoolean();
+
+            Assert.True(result);
         }
     }
 }
