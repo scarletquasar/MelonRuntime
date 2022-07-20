635cc0333d45f9b162a3e129ae9928890920a062
Victória Rose <70824102+victoriaquasar@users.noreply.github.com>
2022-07-20 01:52:08 -0300

    chore(fixes/refactor): fixes in execution handler and refactored NewCommand
    - "run" and "load" command was not handling errors correctly because they weren't using "Helpers.ExecuteWithHandler"
    - Refactored "NewCommand" to consistently implement new project files in a scalable maneer

--- projects/melon-runtime/Melon/Commands/NewCommand.cs
+++ projects/melon-runtime/Melon/Commands/NewCommand.cs
@@ -10,42 +10,28 @@ namespace Melon.Commands
         {
             var currentPath = Environment.CurrentDirectory + (arguments.Length > 0 ? string.Join("", arguments) : "");
 
-            CLNConsole.Write("> ", ConsoleColor.DarkRed);
-            CLNConsole.Write("Creating project item ", ConsoleColor.Yellow);
-            CLNConsole.Write("src/index.ts", ConsoleColor.Cyan);
-            Console.WriteLine();
-
             Directory.CreateDirectory(currentPath + "/src/");
-            File.WriteAllText(currentPath + "/src/index.ts", Resources.NewProjectIndex);
 
+            List<Tuple<string, string, string>> projectFiles = new()
+            {
+                new("src/index.ts", currentPath + "/src/index.ts", Resources.NewProjectIndex),
+                new(".babelrc", currentPath + "/.babelrc", Resources.NewProjectBabelRc),
+                new("package.json", currentPath + "/package.json", Resources.NewProjectPackageInfo),
+                new("tsconfig.json", currentPath + "/tsconfig.json", Resources.NewProjectTsconfig),
+                new(".gitignore", currentPath + "/.gitignore", Resources.NewProjectGitIgnore)
+            };
 
+            projectFiles.ForEach(file => CreateProjectFile(file.Item1, file.Item2, file.Item3));
+        }
+
+        private static void CreateProjectFile(string item, string path, string content)
+        {
             CLNConsole.Write("> ", ConsoleColor.DarkRed);
             CLNConsole.Write("Creating project item ", ConsoleColor.Yellow);
-            CLNConsole.Write(".babelrc", ConsoleColor.Cyan);
+            CLNConsole.Write(item, ConsoleColor.Cyan);
             Console.WriteLine();
 
-            File.WriteAllText(currentPath + "/.babelrc", Resources.NewProjectBabelRc);
-
-            CLNConsole.Write("> ", ConsoleColor.DarkRed);
-            CLNConsole.Write("Creating project item ", ConsoleColor.Yellow);
-            CLNConsole.Write("package.json", ConsoleColor.Cyan);
-            Console.WriteLine();
-
-            File.WriteAllText(currentPath + "/package.json", Resources.NewProjectPackageInfo);
-
-            CLNConsole.Write("> ", ConsoleColor.DarkRed);
-            CLNConsole.Write("Creating project item ", ConsoleColor.Yellow);
-            CLNConsole.Write("tsconfig.json", ConsoleColor.Cyan);
-            Console.WriteLine();
-
-            File.WriteAllText(currentPath + "/tsconfig.json", Resources.NewProjectTsconfig);
-
-            CLNConsole.Write("> ", ConsoleColor.DarkRed);
-            CLNConsole.Write("Creating project item ", ConsoleColor.Yellow);
-            CLNConsole.Write(".gitignore", ConsoleColor.Cyan);
-            Console.WriteLine();
-
-            File.WriteAllText(currentPath + "/.gitignore", Resources.NewProjectGitIgnore);
+            File.WriteAllText(path, content);
         }
     }
 }
