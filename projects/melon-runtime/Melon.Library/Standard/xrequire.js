cfbcd5abbb8db2fd4ebeb81ad4d71dc09411d82d
Victória Rose <70824102+victoriaquasar@users.noreply.github.com>
2022-07-28 00:04:01 -0300

    chore: removed unused comment

--- projects/melon-runtime/Melon.Library/Standard/xrequire.js
+++ projects/melon-runtime/Melon.Library/Standard/xrequire.js
@@ -6,7 +6,6 @@
             return __xrequire_dotnet_internal__(attributes[1]);
 
         case "dotnet-external":
-            /* TODO: Check if the target loaded file actually exists */
             return __xrequire_dotnet_external__(attributes[1]);
     }
 }
