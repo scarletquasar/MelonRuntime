702c081a06f29de7b5e871a06534a829089c8108
Victória Rose <70824102+victoriaquasar@users.noreply.github.com>
2022-08-24 21:52:04 -0300

    chore(melon-types): implemented "InteropResult" to prevent any in interop end-to-end operations

--- projects/melon-types/types/dotnet/Realm.ts
+++ projects/melon-types/types/dotnet/Realm.ts
@@ -1,10 +1,11 @@
 import { DotnetInstanceExpression } from "./DotnetInstanceExpression";
+import { InteropResult } from "./InteropResult";
 
 interface Realm {
     name: string;
     setValue(name: string, value: any): void;
     setInstance(name: string, expression: DotnetInstanceExpression, ...parameters: any): void;
-    get(name: string): any;
+    get(name: string): InteropResult;
 }
 
 export { Realm }
