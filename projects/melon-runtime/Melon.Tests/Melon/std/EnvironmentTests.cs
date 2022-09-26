f74b869756351d23bc76bc61f9d6527b47db3c8b
Victória Rose <70824102+victoriaquasar@users.noreply.github.com>
2022-09-25 23:43:11 -0300

    tests(wip): tests development
    + Finished `Melon.std.system` tests
    + In progress `Melon.std.environment` tests

---
+++ projects/melon-runtime/Melon.Tests/Melon/std/EnvironmentTests.cs
@@ -0,0 +1,46 @@
+﻿using Jint;
+using Melon.Engine.Builders;
+using System;
+using Xunit;
+
+namespace Melon.Tests.Melon.std
+{
+    public class EnvironmentTests
+    {
+        private readonly Jint.Engine _engine;
+
+        public EnvironmentTests()
+        {
+            var builder = new EngineBuilder();
+            builder.Load("Bundle/core");
+            _engine = builder.Build();
+        }
+
+        [Fact(DisplayName = "Melon.std.environment.baseDirectory should have the correct value")]
+        public void EnvironmentBaseDirectoryShouldHaveTheCorrectValue()
+        {
+            var script = @"Melon.std.environment.baseDirectory";
+            var result = _engine.Evaluate(script).AsString();
+
+            Assert.Equal(Environment.CurrentDirectory, result);
+        }
+
+        [Fact(DisplayName = "Melon.std.environment.setEnvironmentVariable should work correctly")]
+        public void EnvironmentSetEnvironmentVariableShouldWorkCorrectly()
+        {
+
+        }
+
+        [Fact(DisplayName = "Melon.std.environment.getEnvironmentVariable should work correctly")]
+        public void EnvironmentGetEnvironmentVariableShouldWorkCorrectly()
+        {
+
+        }
+
+        [Fact(DisplayName = "Melon.std.environment.getEnvironmentVariables should work correctly")]
+        public void EnvironmentGetEnvironmentVariablesShouldWorkCorrectly()
+        {
+
+        }
+    }
+}
