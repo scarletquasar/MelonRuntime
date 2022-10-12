0e96296b95e64317454443ed7f55ef413b69aa91
Victória Rose <70824102+victoriaquasar@users.noreply.github.com>
2022-10-12 03:30:31 -0300

    chore: formatting fixes

--- projects/melon-types/modules/Guards/Guards.ts
+++ projects/melon-types/modules/Guards/Guards.ts
@@ -4,13 +4,13 @@ declare type Guards = {
         isOdd: (number: number) => boolean;
         isInteger: (number: number) => boolean;
         isFloat: (number: number) => boolean;
-    };
+    },
     iterable: {
         isEmptyArray: (array: any[]) => boolean;
         isEmptyObject: (object: Object) => boolean;
-    };
+    },
     string: {
         isNullOrEmpty: (string: string) => boolean;
         isNullOrWhiteSpace: (string: string) => boolean;
-    };
+    },
 }
