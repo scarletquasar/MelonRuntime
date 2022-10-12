2456baeb261e2893f85a33cc611dd6e05040f7d1
Victória Rose <70824102+victoriaquasar@users.noreply.github.com>
2022-10-12 02:54:55 -0300

    port: number, host = this.host

--- projects/melon-types/modules/Http/Http.ts
+++ projects/melon-types/modules/Http/Http.ts
@@ -9,6 +9,68 @@ declare interface HttpResponse {
     statusCode: number;
     ok: boolean;
 }
+
+declare interface HttpRequest {
+    query: Record<string, any>;
+    body: any;
+    headers: Record<string, any>;
+}
+
+declare interface HttpComposedResponse {
+    status: number;
+    response: string;
+    headers: string;
+}
+
+declare interface HttpEndpoint {
+    route: string;
+    method: string;
+    callback: Function;
+}
+
+declare type HttpCallbackFunction = 
+    (request: HttpRequest) => string | HttpComposedResponse;
+
+declare type HttpAsyncCallbackFunction = 
+    (request: HttpRequest) => Promise<string> | Promise<HttpComposedResponse>;
+
+declare interface HttpApplication {
+    name: string;
+    host: string;
+    port: number;
+    enableHttps: boolean;
+    getEndpoints: () => HttpEndpoint[];
+    get: (
+        route: string, 
+        callback: HttpCallbackFunction | HttpAsyncCallbackFunction
+    ) => void;
+    post: (
+        route: string, 
+        callback: HttpCallbackFunction | HttpAsyncCallbackFunction
+    ) => void;
+    delete: (
+        route: string, 
+        callback: HttpCallbackFunction | HttpAsyncCallbackFunction
+    ) => void;
+    patch: (
+        route: string, 
+        callback: HttpCallbackFunction | HttpAsyncCallbackFunction
+    ) => void;
+    put: (
+        route: string, 
+        callback: HttpCallbackFunction | HttpAsyncCallbackFunction
+    ) => void;
+    options: (
+        route: string, 
+        callback: HttpCallbackFunction | HttpAsyncCallbackFunction
+    ) => void;
+    head: (
+        route: string, 
+        callback: HttpCallbackFunction | HttpAsyncCallbackFunction
+    ) => void;
+    listen: (port: number, host?: string) => void;
+    run: () => void;
+}
  
 declare interface Http {
     request: (
@@ -23,4 +85,10 @@ declare interface Http {
         body?: Record<string, any>, 
         headers?: Record<string, any>
     ) => Promise<HttpResponse>;
+    app: (options: {
+        name: string, 
+        host: string, 
+        port: number, 
+        enableHttps: boolean 
+    }) => HttpApplication;
 }
