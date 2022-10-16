e634ee85e2102aa727c49f612714453a7042cc88
Victória Rose <70824102+victoriaquasar@users.noreply.github.com>
2022-10-16 01:15:11 -0300

    feat: implemented async.nextTick() in async methods

--- projects/melon-core/src/partials/modules/fs/_copyFileAsync.ts
+++ projects/melon-core/src/partials/modules/fs/_copyFileAsync.ts
@@ -1,4 +1,7 @@
+import { _std } from "../std/_std";
+
 async function _copyFileAsync(from: string, to: string) {
+    await _std.async.nextTick();
     _$internalBinding["CopyFile"](from, to, true);
 }
 
