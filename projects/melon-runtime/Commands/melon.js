79915c3af083a507cb8f2dc93cfb56f74b42e367
Victória Rose <70824102+victoriaquasar@users.noreply.github.com>
2022-11-04 15:10:01 -0300

    feat: implemented auto-updater + no update command

--- projects/melon-runtime/Commands/melon.js
+++ projects/melon-runtime/Commands/melon.js
@@ -1,16 +1,28 @@
 #! /usr/bin/env node
 import { fileURLToPath } from 'url';
-import { spawn } from 'child_process';
+import { spawn, spawnSync } from 'child_process';
 import path from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const args = process.argv.slice(2);
-
-const outputDirectory = __dirname.replace('Commands', 'Output');
 const spawnOptions = { stdio: "inherit" };
+let args = process.argv.slice(2);
 
-const melon = spawn('dotnet', ["exec", path.join(outputDirectory, "Melon.dll"), ...args], spawnOptions);
+//Commands implementation
+if(!process.argv.includes("--ignore-update")) {
+    spawnSync(/^win/.test(process.platform) ? 'npm.cmd' : 'npm', ['install', 'melon-runtime@^2.x.x', '-g']);
+    
+    const instance = spawn(/^win/.test(process.platform) ? 'npx.cmd' : 'npx', ['--ignore-update', 'melon', ...args], spawnOptions);
+    instance.on('data', console.log);
+}
 
-melon.on('data', console.log);
+if(process.argv.includes("--ignore-update")) {
+    args = args.filter(x => !x.startsWith("--"));
+
+    //Dotnet Melon implementation
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const outputDirectory = __dirname.replace('Commands', 'Output');
+    const melon = spawn('dotnet', ["exec", path.join(outputDirectory, "Melon.dll"), ...args], spawnOptions);
+
+    melon.on('data', console.log);
+}
