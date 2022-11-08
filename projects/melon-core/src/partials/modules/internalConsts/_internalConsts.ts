3044a432b1539eb85218cdb7f68f17a411813076
Victória Rose <70824102+victoriaquasar@users.noreply.github.com>
2022-11-08 12:51:16 -0300

    refactor: removed unused internal const

--- projects/melon-core/src/partials/modules/internalConsts/_internalConsts.ts
+++ projects/melon-core/src/partials/modules/internalConsts/_internalConsts.ts
@@ -1,6 +1,4 @@
 const _internalConsts = {
-    //Enumerable
-    ENUMERABLE_EXCEEDED_CAPACITY: "Exceeded limit capacity for this Enumerable",
     //dotnet.types.[numeric type]
     INVALID_NUMBER_CAPACITY: "The number capacity is invalid for that type",
     //data.compare
