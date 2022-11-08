d4afdde9810097a14ec39587babf79440505fc54
Victória Rose <70824102+victoriaquasar@users.noreply.github.com>
2022-11-08 12:20:27 -0300

    refactor: getEnv and Melon globals

--- projects/melon-core/src/index.ts
+++ projects/melon-core/src/index.ts
@@ -9,11 +9,12 @@ import { _guards } from "./partials/modules/guards/_guards"
 import { _http } from "./partials/modules/http/_http"
 import { _std } from "./partials/modules/std/_std"
 import { _crypto } from "./partials/statics/_Crypto"
-import { $ } from "./partials/utils/$"
-import { addPrototypeExtension } from "./partials/utils/addPrototypeExtension"
-import { getEnv } from "./partials/utils/getEnv"
+import { $ } from "./partials/utils/generic/$"
+import { addPrototypeExtension } from "./partials/utils/generic/addPrototypeExtension"
+import { getEnv } from "./partials/utils/environment/getEnv"
 
 getEnv();
+addPrototypeExtension(Object, "$", $);
 
 const Melon = {
     std: _std,
@@ -27,18 +28,16 @@ const Melon = {
     Version: _Version
 }
 
-addPrototypeExtension(Object, "$", $);
-
-globalThis.console = Melon.console as any;
+globalThis.console = <any>Melon.console;
 globalThis.crypto = Melon.crypto;
 globalThis.fs = Melon.fs;
 
-globalThis.setTimeout = Melon.std.time.setTimeout as any;
-globalThis.setInterval = Melon.std.time.setInterval as any;
+globalThis.setTimeout = <any>Melon.std.time.setTimeout;
+globalThis.setInterval = <any>Melon.std.time.setInterval;
 globalThis.fetch = Melon.http.fetch;
 
-globalThis.Map = _Map as any;
-globalThis.Set = _Set as any;
+globalThis.Map = <any>_Map;
+globalThis.Set = <any>_Set;
 
 globalThis.Melon = Melon;
 
