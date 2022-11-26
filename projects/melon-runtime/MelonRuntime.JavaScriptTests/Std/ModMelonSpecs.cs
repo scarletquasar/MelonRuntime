d3e5068fb566f219c846e69b8d06ef7e1146d389
Victória Rose <70824102+victoriaquasar@users.noreply.github.com>
2022-11-26 18:48:02 -0300

    tests(wip): base for ModMelonSpecs

---
+++ projects/melon-runtime/MelonRuntime.JavaScriptTests/Std/ModMelonSpecs.cs
@@ -0,0 +1,23 @@
+﻿using Jint.Native;
+using MelonRuntime.Abstractions.Generic;
+using System;
+using Xunit;
+
+namespace MelonRuntime.JavaScriptTests.Std
+{
+    public class ModMelonSpecs
+    {
+        private readonly IMelon<JsValue> _melon;
+
+        public ModMelonSpecs()
+        {
+            _melon = MelonGenerator.Get();
+        }
+
+        [Fact]
+        public void CurrentVersionShouldHaveTheCorrectValue()
+        {
+
+        }
+    }
+}
