7f324dedd01bcfb8f6c14e811d89d1ad963be921
Victória Rose <70824102+victoriaquasar@users.noreply.github.com>
2022-12-03 13:04:27 -0300

    feat(wip, breaking change): implementation of project items generator

--- projects/melon-runtime/MelonRuntime.ProjectGenerator/Generator.cs
+++ projects/melon-runtime/MelonRuntime.ProjectGenerator/Generator.cs
@@ -1,14 +1,67 @@
 ﻿using MelonRuntime.ProjectGenerator;
 using Newtonsoft.Json;
 using Microsoft.VisualBasic.FileIO;
+using Cli.NET.Tools;
+using MelonRuntime.ProjectGenerator.Models;
 
 namespace MelonRuntime.ProjectGenerator
 {
-    public class Generator
+    public static class Generator
     {
-        public async Task Create(string schemaName, string path, bool verbose = false)
+        private static async Task CreateProjectItem(string item, string path, string content, bool verbose)
         {
+            if(verbose)
+            {
+                CLNConsole.Write("> ", ConsoleColor.Magenta);
+                CLNConsole.Write("Creating project item ", ConsoleColor.Yellow);
+                CLNConsole.Write(item, ConsoleColor.Cyan);
+                Console.WriteLine();
+            }
 
+            await File.WriteAllTextAsync(path, content);
+        }
+
+        private static Schema GetTypeScriptScheme()
+        {
+            return new()
+            {
+                new(".env", "./.env", Resources.DotEnv),
+                new("src/index.ts", "./src/index.ts", Resources.NewProjectTsIndex),
+                new(".babelrc", "./.babelrc", Resources.NewProjectTsBabelRc),
+                new("package.json", "./package.json", Resources.NewProjectTsPackageInfo),
+                new("tsconfig.json", "./tsconfig.json", Resources.NewProjectTsconfig),
+                new(".gitignore", "./.gitignore", Resources.NewProjectTsGitIgnore),
+                new(
+                    "webpack.config.production.js",
+                    "./webpack.config.production.js",
+                    Resources.NewProjectWebpackConfigProduction
+                ),
+                new(
+                    "webpack.config.development.js",
+                    "./webpack.config.development.js",
+                    Resources.NewProjectWebpackConfigDevelopment
+                )
+            };
+        }
+
+        private static Schema GetJavaScriptScheme()
+        {
+            return new()
+            {
+                new(".env", "./.env", Resources.DotEnv),
+                new("src/index.js", "./src/index.js", Resources.NewProjectJsIndex),
+                new("package.json", "./package.json", Resources.NewProjectJsPackageInfo),
+                new(
+                    "webpack.config.production.js",
+                    "./webpack.config.production.js",
+                    Resources.NewProjectWebpackConfigProduction
+                ),
+                new(
+                    "webpack.config.development.js",
+                    "./webpack.config.development.js",
+                    Resources.NewProjectWebpackConfigDevelopment
+                )
+            };
         }
     }
 }
