23058b280c6af3b512e99733a5eccee4a6439e71
Victória Rose <70824102+victoriaquasar@users.noreply.github.com>
2022-12-07 00:42:42 -0300

    tests: implemented Melon.std.time specs

---
+++ projects/melon-runtime/MelonRuntime.JavaScriptTests/Std/ModTimeSpecs.cs
@@ -0,0 +1,47 @@
+﻿using Jint;
+using Jint.Native;
+using MelonRuntime.Abstractions.Generic;
+using Xunit;
+
+namespace MelonRuntime.JavaScriptTests.Std
+{
+    public class ModTimeSpecs
+    {
+        private readonly IMelon<JsValue> _melon;
+
+        public ModTimeSpecs()
+        {
+            _melon = MelonGenerator.Get();
+        }
+
+        [Fact]
+        public void SetTimeoutShouldWorkCorrectly()
+        {
+            var script = @"
+                let result = 0;
+                setTimeout(() => result++, 10);
+                while(true) { if(result > 0) { break; } }
+                result;
+            ";
+
+            var result = _melon.EvaluateInstructionsDirectly(script).AsNumber();
+
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void SetIntervalShouldWorkCorrectly()
+        {
+            var script = @"
+                let result = 0;
+                setInterval(() => result++, 10);
+                while(true) { if(result > 1) { break; } }
+                result;
+            ";
+
+            var result = _melon.EvaluateInstructionsDirectly(script).AsNumber();
+
+            Assert.Equal(2, result);
+        }
+    }
+}
