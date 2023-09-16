e7ef920e15e401bcc3f7ceef1e0e2ef0d85eef6d
Scarlet Rose <70824102+scarletquasar@users.noreply.github.com>
2023-09-16 04:24:59 -0300

    chore: removed unused bloatware

--- projects/core/logic/api/errors/InvalidArgumentError.ts
+++
@@ -1,6 +0,0 @@
-export class InvalidArgumentError extends Error {
-    constructor(...causes: string[]) {
-        super(`Invalid arguments: ${causes.join("; ")}`);
-        this.name = "InvalidArgumentError";
-    }
-}
